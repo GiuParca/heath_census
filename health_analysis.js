2deabce4dfa9b4dd60ae98618db0c5d43d9403ae
GiuParca <giuliaparca@gmail.com>
2024-08-01 03:49:00 -0400

    adding Patients

---
+++ health_analysis.js
@@ -0,0 +1,52 @@
+const addPatientButton = document.getElementById('addPatient');
+const report = document.getElementById('report');
+const btnSearch = document.getElementById('btnSearch');
+const patients = [];
+
+function addPatient(){
+    const name = document.getElementById('name').value;
+    const gender = document.querySelector('input[name=gender]:checked');
+    const age = document.getElementById('age').value;
+    const condition = document.getElementById('condition').value;
+
+    if(name && gender && age && condition){
+        patients.push({name,gender:gender.value,age,condition});
+        resetForm();
+        generateReport();
+    }
+}
+
+addPatientButton.addEventListener('click', resetForm)
+
+function resetForm(){
+    document.getElementById('name').value = "";
+    document.querySelector('input[name=gender]:checked').checked = false;
+    document.getElementById('age').value = "";
+    document.getElementById('condition').value = "";
+}
+
+function generateReport(){
+    const numPatients = patients.length;
+    const conditionsCount = {
+        Diabetes: 0,
+        Thyroid: 0,
+        "HIght Blood Pressure": 0,
+    };
+    const genderConditionsCount = {
+        Male:{
+            Diabetes: 0,
+            Thyroid: 0,
+            "HIght Blood Pressure": 0,
+        },
+        Female:{
+            Diabetes: 0,
+            Thyroid: 0,
+            "HIght Blood Pressure": 0,
+        },
+    };
+
+    for(const patient of patients){
+        conditionsCount[patient.condition]++;
+        genderConditionsCount[patient.condition][patient.gender]++;
+    }
+}
